9252a591718ecd965c98384630c10ffdc587bb39
funkerkat <funkerkat@gmail.com>
2022-03-25 15:09:10 +0300

    commit 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hello my app 1");
+        System.out.println("hello my app 3");
     }
 }
